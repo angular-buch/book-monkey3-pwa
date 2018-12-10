4ccb3a6e62c350a19df4d33df4f8572572599a25
The Buildbot <buildbot@angular2buch.de>
2018-12-10 23:00:43 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/2a5bb7db573f2432d3b32d9b330a22dc3e83a141
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/178

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,4 +28,8 @@ import { TokenInterceptor } from './shared/token-interceptor';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeDe);
+  }
+}
