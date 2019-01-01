70641c3f3350337587800166b140706083c95309
The Buildbot <buildbot@angular2buch.de>
2019-01-01 21:27:08 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/60000b8e6e20b6109c36cf1096151988a90d142a
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/1787

---
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -0,0 +1,80 @@
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+
+import { BookListComponent } from './book-list.component';
+import { BookStoreService } from '../../shared/book-store.service';
+import { Book } from '../../shared/book';
+
+@Component({ template: 'Dummy' })
+class DummyDetailsComponent { }
+
+@Component({
+  selector: 'bm-book-list-item',
+  template: 'Dummy'
+})
+class DummyBookListItemComponent {
+  @Input() book: Book;
+}
+
+describe('BookListComponent', () => {
+  let component: BookListComponent;
+  let fixture: ComponentFixture<BookListComponent>;
+  let location: Location;
+
+  const expectedBooks = [
+    {
+      isbn: '111',
+      title: 'Book 1',
+      authors: [],
+      published: new Date()
+    },
+    {
+      isbn: '222',
+      title: 'Book 2',
+      authors: [],
+      published: new Date()
+    }
+  ];
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        BookListComponent,
+        DummyBookListItemComponent,
+        DummyDetailsComponent
+      ],
+      providers: [{
+        provide: BookStoreService,
+        useValue: { getAll: () => of(expectedBooks) }
+      }],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: ':isbn', component: DummyDetailsComponent }
+        ])
+      ]
+    });
+  }));
+
+  beforeEach(async(() => {
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(BookListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should display books', () => {
+    expect(component.books.length).toBe(2);
+    expect(component.books[0].isbn).toBe('111');
+    expect(component.books[1].isbn).toBe('222');
+  });
+
+  it('should navigate to details page by ISBN', async(() => {
+    fixture.nativeElement.querySelector('bm-book-list-item').click();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toEqual('/111');
+    });
+  }));
+});
