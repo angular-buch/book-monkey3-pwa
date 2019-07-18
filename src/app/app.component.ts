39b410ec87516a1d5361beb231122ad537d8a48d
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-07-18 23:43:52 +0200

    improves service worker (Show Update hint, cache book data)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'bm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe((evt) => {
+        const updateApp = window.confirm(`
+          Ein Update ist verfügbar (${evt.current.appData['version']} => ${evt.available.appData['version']}).
+          Änderungen: ${evt.current.appData['changelog']}
+          Wollen Sie das Update jetzt installieren?
+        `);
+        if (updateApp) { window.location.reload(); }
+      });
+    }
+  }
+}
